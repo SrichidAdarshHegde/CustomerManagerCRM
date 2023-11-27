6ed394adfe0fee2e977206d8e205fd94301ac1f2
kruthi5566@gmail.com <kruthi5566@gmail.com>
2023-11-27 18:19:25 +0530

    k

--- src/app/Components/machine-list/machine-list.component.ts
+++ src/app/Components/machine-list/machine-list.component.ts
@@ -25,6 +25,20 @@ export class MachineListComponent implements OnInit{
   viewcustomer:any;
   machineID:any;
   customerID: any;
+  EditItem:any;
+  deleteItem:any;
+  combinedList: any[] = [];
+  GetAllMachines: any;
+  selectedPerticularCustomer: any;
+  contactDetails: any;
+  clusterlist: any;
+  routelist: any;
+  regionlist: any;
+  zonelist: any;
+  editselectedcluster: any;
+  editselectedroute: any;
+  editselectedregion: any;
+  editselectedZone: any;
   constructor(private regSv:RegistrationService,  private masterSv: MasterService,){
 
   }
@@ -54,14 +68,88 @@ export class MachineListComponent implements OnInit{
       }
     });
   }
+
+  getCluster() {
+    this.masterSv.getCluster().subscribe((response: any) => {
+      this.clusterlist = response;
+      console.log(this.clusterlist)
+    })
+  }
+  getRoute() {
+    this.masterSv.getRoute().subscribe((response: any) => {
+      this.routelist = response;
+      console.log(this.routelist)
+    })
+  }
+  getRegion() {
+    this.masterSv.getRegion().subscribe((response: any) => {
+      this.regionlist = response;
+      console.log(this.regionlist)
+    })
+  }
+  getZone() {
+    this.masterSv.getZone().subscribe((response: any) => {
+      this.zonelist = response;
+      console.log(this.zonelist)
+    })
+  }
+
+
+  onSelectCluster(data: any) {
+    this.editselectedcluster = data.target.value;
+  }
+  onSelectRoute(data: any) {
+    this.editselectedroute = data.target.value;
+  }
+  onSelectRegion(data: any) {
+    this.editselectedregion = data.target.value;
+  }
+  onSelectZone(data: any) {
+    this.editselectedZone = data.target.value;
+  }
+
+  mergeLists(data: any) {
+    this.customerID = data.customerID;
+  
+    // Fetch both customer and machine lists
+    this.regSv.getCustomer().subscribe((customerResponse: any) => {
+      this.customerList = customerResponse;
+  
+      if (this.customerList.length !== 0) {
+        this.exporting = true;
+      }
+      console.log("Customer List");
+      console.log(this.customerList);
+  
+      // Fetch machine list after getting the customer list
+      this.regSv.GetAllMachines().subscribe((machineResponse: any) => {
+        this.machineList = machineResponse;
+  
+        console.log("Machine List");
+        console.log(this.machineList);
+  
+        // Combine customer and machine lists
+        this.combinedList = [this.customerList, this.machineList];
+      });
+    });
+  }
+
+
+
   onSelectCompany(data: any) {
     this.customerID = data.customerID;
     this.regSv
       .getPerticularMachine(this.customerID)
       .subscribe((response: any) => {
         this.machineList = response
+        this.customerList= response
       });
   }
+
+  
+
+
+
   EditMachine(data:any)
   {
     this.machineID=data.id;
