4fafe3ea698eabf652789252b8e3534bb56d485b
jobs-zone <adarsh@blocdriveprivateltd.onmicrosoft.com>
2024-01-18 16:44:25 +0530

    Adi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -109,6 +109,7 @@ import { NumtowordsPipe } from 'src/numtowords.pipe';
 import { TravelSheetComponent } from './Components/travel-sheet/travel-sheet.component';
 import { Quotation2015indollorComponent } from './Components/QM TEMPLATES/quotation2015indollor/quotation2015indollor.component';
 import { RapidIVMCComponent } from './Components/QM TEMPLATES/rapid-i-vmc/rapid-i-vmc.component';
+import { PurposeJobTimeMasterComponent } from './Components/Masters/purpose-job-time-master/purpose-job-time-master.component';
 
 @NgModule({
   declarations: [
@@ -203,6 +204,7 @@ import { RapidIVMCComponent } from './Components/QM TEMPLATES/rapid-i-vmc/rapid-
 RapidIsparesComponent,
 Quotation2015indollorComponent,
 RapidIVMCComponent,
+PurposeJobTimeMasterComponent
 
   ],
   imports: [
