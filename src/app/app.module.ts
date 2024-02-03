47933ab4793ec164f8a8a7ae81bec7c39ebf16ad
jobs-zone <adarsh@blocdriveprivateltd.onmicrosoft.com>
2024-02-03 10:24:59 +0530

    Adi 03-02-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -110,6 +110,8 @@ import { TravelSheetComponent } from './Components/travel-sheet/travel-sheet.com
 import { Quotation2015indollorComponent } from './Components/QM TEMPLATES/quotation2015indollor/quotation2015indollor.component';
 import { RapidIVMCComponent } from './Components/QM TEMPLATES/rapid-i-vmc/rapid-i-vmc.component';
 import { PurposeJobTimeMasterComponent } from './Components/Masters/purpose-job-time-master/purpose-job-time-master.component';
+import { NumtowordsPipe } from './numtowords.pipe';
+import { SearchPipe } from './search.pipe';
 //import { NumtowordsPipe } from './numtowords.pipe';
 
 
@@ -161,7 +163,7 @@ import { PurposeJobTimeMasterComponent } from './Components/Masters/purpose-job-
     TripSheetComponent,
     LocationdetailsComponent,
     LocationdetailslistComponent,
-    //SearchPipe,
+    SearchPipe,
     QuotationmanagerComponent,
     MailquotationtemplateComponent,
     BrochureComponent,
@@ -209,7 +211,7 @@ Quotation2015indollorComponent,
 RapidIVMCComponent,
 PurposeJobTimeMasterComponent,
 PurposeJobTimeMasterComponent,
-//NumtowordsPipe,
+NumtowordsPipe,
   ],
   imports: [
     BrowserModule,
